55c96ca9f8dbadcb2820d0db3c2e909935c5b638
Nam Văn <nguyenvannam@gmail.com>
2023-08-24 18:10:37 +0700

    kk

--- frontend/src/app/pages/wishslit/wishslit.component.ts
+++ frontend/src/app/pages/wishslit/wishslit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-wishslit',
@@ -24,11 +25,28 @@ export class WishslitComponent implements OnInit {
 
 
   removeItem(id:any, idproduct:any){
-    this.apiService.deletFavorite(idproduct).subscribe((res:any) => {
-      console.log(res)
-    });
-   this.removeFavorie(this.data, id);
-   
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your product has been deleted.',
+          'success'
+        )
+        this.apiService.deletFavorite(idproduct).subscribe((res:any) => {
+        });
+        this.removeFavorie(this.data, id);
+      }else {
+        return false;
+      }
+    })
   }
 
   removeFavorie(data:any, idx:number) {
